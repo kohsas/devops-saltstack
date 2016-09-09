dec61a15b5adf62bba50d776c25601da34859adc
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2016-09-09 15:20:17 +0000

    cleanup

--- scripts/bootstrap_salt_minion.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-apt-get update
-curl -L https://bootstrap.saltstack.com | sudo sh
-echo "master: saltmaster" | sudo tee -a /etc/salt/minion
-# disable salt-master  service, if it exists.    
-if service --status-all | grep -Fq 'salt-master'; then    
-  sudo service salt-master stop    
-fi
--- top.sls
+++
@@ -1,4 +0,0 @@
-base:
-  '*':
-    - common
-    - software.docker
