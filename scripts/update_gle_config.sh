55d08d34641a8aae5271a7c3f8f48d13390bcfd0
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2016-09-16 15:24:05 +0000

    what i need to do for updating the master with the providers

---
+++ scripts/update_gle_config.sh
@@ -0,0 +1,4 @@
+sudo cp cloud/cloud.providers.d/gle.conf  /etc/salt/cloud.providers.d/
+sudo cp cloud/cloud.profiles.d/gle.conf  /etc/salt/cloud.profiles.d/
+sudo cp cloud/cloud.providers.d/Salt\ Stack-5c33ab24a647.json  /etc/salt/
+sudo chmod 0600 '/etc/salt/Salt Stack-5c33ab24a647.json'
