68e6b3b8b6749797794fc340344b02c4ccab902f
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2016-09-17 13:30:14 +0000

    modifying update gle config to a shell script

--- scripts/update_gle_config.sh
+++ scripts/update_gle_config.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 sudo cp cloud/cloud.providers.d/gle.conf  /etc/salt/cloud.providers.d/
 sudo cp cloud/cloud.profiles.d/gle.conf  /etc/salt/cloud.profiles.d/
 sudo cp cloud/cloud.providers.d/Salt\ Stack-5c33ab24a647.json  /etc/salt/
