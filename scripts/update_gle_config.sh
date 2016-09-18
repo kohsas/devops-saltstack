7628b188adab656d49c5c4df5b2090762c65a636
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2016-09-18 08:42:13 +0000

    dynamically generating saltmaster in profiles

--- scripts/update_gle_config.sh
+++ scripts/update_gle_config.sh
@@ -6,7 +6,16 @@ die () {
     exit 1
 }
 
-sudo cp cloud/cloud.providers.d/gle.conf  /etc/salt/cloud.providers.d/
-sudo cp cloud/cloud.profiles.d/gle.conf  /etc/salt/cloud.profiles.d/
-sudo cp cloud/keys/gle-service-account-private-key.json  /etc/salt/
+nodename=`uname -n`
+basedir=$1
+providers=$basedir/cloud/cloud.providers.d/gle.conf 
+profiles=$basedir/cloud/cloud.profiles.d/gle.conf 
+profiles_tmp="/tmp/tmp_gle.conf"
+echo "[INFO] copying $providers"
+sudo cp $providers  /etc/salt/cloud.providers.d/
+
+echo "[INFO] copying $profiles"
+echo -e "salt-master:\n  minion:$nodename\n\n$(cat  $profiles)" > $profiles_tmp
+sudo cp $profiles_tmp  /etc/salt/cloud.profiles.d/
+sudo cp $basedir/cloud/keys/gle-service-account-private-key.json  /etc/salt/
 sudo chmod 0600 '/etc/salt/gle-service-account-private-key.json'
