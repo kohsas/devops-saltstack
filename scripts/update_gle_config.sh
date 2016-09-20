891808c944f5502d6577bb9fc37022a2ed8162a9
Ashok Satyanarayana <ashok.satyanarayana@gmail.com>
2016-09-20 17:23:46 +0000

    space fixes

--- scripts/update_gle_config.sh
+++ scripts/update_gle_config.sh
@@ -16,8 +16,10 @@ echo "[INFO] copying $providers"
 sudo cp $providers  /etc/salt/cloud.providers.d/
 
 echo "[INFO] copying $profiles"
-echo -e "$(cat $pre_profile)  minion:\n    master: $nodename\n\n$(cat  $profiles)" > $profiles_tmp
+echo -e "$(cat $pre_profile)minion:\n    master: $nodename\n\n$(cat  $profiles)" > $profiles_tmp
 sudo cp $profiles_tmp  /etc/salt/cloud.profiles.d/gle.conf
 echo "[INFO] copying service account key"
 sudo cp $basedir/cloud/keys/gle-service-account-private-key.json  /etc/salt/
 sudo chmod 0600 '/etc/salt/gle-service-account-private-key.json'
+
+
